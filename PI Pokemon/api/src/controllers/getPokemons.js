b37ab3c9d8e7267b88f8f96d10221e7a4e0d2a3e
Victor Sepulveda <victorseva123@gmail.com>
2024-02-20 11:24:16 -0500

    Creacion de getPokemons-Id 20/02/24

--- PI Pokemon/api/src/controllers/getPokemons.js
+++ PI Pokemon/api/src/controllers/getPokemons.js
@@ -54,10 +54,22 @@ const getPokemonApi = async (req, res) => {
             let e = obj.data
                 let pokemon = {
                     id: e.id,
-                    
-                }
+                    name: (e.name).charAt(0).toUpperCase() + (e.name).slice(1),
+                    life: e.stats[0].base_stat,
+                    stroke: e.stats[1].base_stat,
+                    defending: e.stats[2].base_stat,
+                    speed: e.stats[5].base_stat,
+                    height: e.height,
+                    weight: e.weight,
+                    imageDefault: e.sprites.other.dream_world.front_default,
+                    types: e.types.map((t) => t.types.name),
+                    createdDB: false
+                };
+                return pokemon;
         })
+        return pokemonsApi
     } catch (error) {
-        
+        res.status(400).json({error: error.messaje})
     }
 }
+module.export = getPokemons
