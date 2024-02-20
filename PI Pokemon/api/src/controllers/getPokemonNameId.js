8e0d7ceab7ea4d55145c5104217f84ca7cf9c23e
Victor Sepulveda <victorseva123@gmail.com>
2024-02-20 12:55:34 -0500

    Creacion de getPokemonNameId 20/02/24

--- PI Pokemon/api/src/controllers/getPokemonNameId.js
+++ PI Pokemon/api/src/controllers/getPokemonNameId.js
@@ -15,12 +15,46 @@ const getPokemonNameId = async (req, res) => {
             urlGet = `https://pokeapi.co/api/v2/pokemon/${name}`
         };
         
-        const {data} = await axios.getAdapter(urlGet)
+        const {data} = await axios.get(urlGet)
 
         const pokemon = {
-            
-        }
+            id: data.id,
+            name: (data.name).charAt(0).toUpperCase() + (data.name).slice(1),
+            life: data.stats[0].base_stat,
+            stroke: data.stats[1].base_stat,
+            defending: data.stats[2].base_stat,
+            speed: data.stats[5].base_stat,
+            height: data.height,
+            weight: data.weight,
+            imageDefault: data.sprites.other.dream_world.front_default,
+            types: data.types.map((t) => t.types.name)
+        };
+        console.log(pokemon);
+        return pokemon
     } catch (error) {
-        
+        res.status(404).json({error: error.messaje})
     }
 }
+
+const getPokeDbId = async (id, res) => {
+    try {
+        console.log("ingresa a buscarlo en DB");
+            const {dataValues} = await Pokemon.findByPk(id, {
+                include:{
+                    model: Type,
+                    attributes: ["name"],
+                    through: {attributes: []}
+                }
+            }) 
+            dataValues.name = (dataValues.name).charAt(0).toUpperCase() + (dataValues.name).slice(1);
+            dataValues.types = dataValues.types.map(e => e.name);
+
+            console.log(dataValues);
+            return dataValues
+
+    } catch (error) {
+        res.status(404).json({error: error.messaje})
+    }
+}
+
+module.export = getPokemonNameId
