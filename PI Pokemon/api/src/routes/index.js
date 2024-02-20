a2d73b0ba6edc6d3b2c588ceecc6e74321227c7b
Victor Sepulveda <victorseva123@gmail.com>
2024-02-20 14:55:16 -0500

    Router index 20/02/24

--- PI Pokemon/api/src/routes/index.js
+++ PI Pokemon/api/src/routes/index.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const RouterPokemon = require("./RouterPokemon");
+const RouterType = require("./RouterType");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.use("/pokemon", RouterPokemon);
+router.use("/tipo", RouterType );
 
 module.exports = router;
