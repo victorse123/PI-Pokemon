e2f244c86772a214edcbb489544a566455757ba8
Victor Sepulveda <victorseva123@gmail.com>
2024-02-20 09:58:13 -0500

    Creacion de getPokemons-Id 20/02/24

--- PI Pokemon/api/src/controllers/getPokemons.js
+++ PI Pokemon/api/src/controllers/getPokemons.js
@@ -28,10 +28,17 @@ const getPokemonDB = async (req, res) => {
                 life: e.life,
                 stroke: e.stroke,
                 defending: e.defending,
-                
+                speed: e.speed,
+                height: e.height,
+                weight: e.weght,
+                imageDefault: e.iamgeDefault,
+                types: e.types.map((t) => t.name),
+                createdDB: e.createdDB
             }
         })
+        console.log(filterPok);
+        return filterPok
     } catch (error) {
-        
+        res.status(400).json({error: error.messaje})
     }
 }
