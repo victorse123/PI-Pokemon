c52ff3f9d6fe80d6f717c12ed9fb3c3148609e3d
Victor Sepulveda <victorseva123@gmail.com>
2024-02-20 13:28:32 -0500

    Models Pokemon 20/02/24

--- PI Pokemon/api/src/models/Pokemon.js
+++ PI Pokemon/api/src/models/Pokemon.js
@@ -4,9 +4,48 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primarykey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
     },
-  });
+    life: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    stroke: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    defending: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    imageDefault: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdDB: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+},{timestamps: false});
 };
