0c48ec6cf7009efcce60d325e70a64485213a5d9
Victor Sepulveda <victorseva123@gmail.com>
2024-02-29 13:52:46 -0500

    front components 29/02/24

---
+++ PI Pokemon/client/src/Components/searchBar/SearchBar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import styles from "../searchBar/SearchBar.module.css"
+import { useDispatch } from "react-redux";
+import { addPokemon } from "../../redux/actions";
+
+const SearchBar = (props)=>{
+    const dispatch = useDispatch()
+    const[pokemon, setPokemon] = useState('')
+    
+    const handleChange =(e)=>{
+        setPokemon(e.target.value)
+    }
+
+    const onSearch =()=>{
+        console.log(pokemon);
+        dispatch(addPokemon(pokemon))
+    }
+    const onSearchEnter =(e)=>{
+        if (e.keyCode === 13) {
+            console.log(pokemon);
+            dispatch(addPokemon(pokemon))
+        }
+    }
+    
+    return(
+    <>
+        <div className={styles.inputGroup}>
+		    <input onChange={handleChange} onKeyDown={onSearchEnter} placeholder="Enter name Pokemon" type="text" />
+		    <button onClick={onSearch} className={styles.submitBtn}><span>BUSCAR</span></button>
+	    </div>
+    </>
+    )
+}
+
+export default SearchBar;
