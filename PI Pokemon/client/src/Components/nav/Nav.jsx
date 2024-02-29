03adcb46dacc8fb71ad60e8384418ce31feabde1
Victor Sepulveda <victorseva123@gmail.com>
2024-02-29 13:43:09 -0500

    front components 29/02/24

--- PI Pokemon/client/src/Components/nav/Nav.jsx
+++ PI Pokemon/client/src/Components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
-import SearchBar from "../searchBar/SearchBar"
-import TypeBar from "../typeBar/TypeBar";
+import SearchBar from "..//searchBar/SearchBar"
+import TypeBar from "..//typeBar/TypeBar";
 import {NavLink, useLocation} from "react-router-dom";
 import styles from "../nav/Nav.module.css"
 
