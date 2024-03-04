d700bcea207ad14f458a08c96fd90402214034cb
Victor Sepulveda <victorseva123@gmail.com>
2024-03-04 13:04:32 -0500

    Pruebas 04/03/24

--- PI Pokemon/client/src/index.jsx
+++ PI Pokemon/client/src/index.jsx
@@ -19,7 +19,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import  store  from "./redux/store/index";
+import  store  from "./Redux/store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
