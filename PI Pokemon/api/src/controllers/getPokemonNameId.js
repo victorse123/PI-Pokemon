ae4994f5a2a57d0ab82cf2a4b836011100ebae1a
Victor Sepulveda <victorseva123@gmail.com>
2024-03-12 17:15:20 -0500

    Detalles 12/03/24

--- PI Pokemon/api/src/controllers/getPokemonNameId.js
+++ PI Pokemon/api/src/controllers/getPokemonNameId.js
@@ -4,21 +4,20 @@ const { Pokemon, Type } = require("../db");
 const getPokemonNameId = async (id) => {
     let urlGet;
     try {
-        console.log('Iniciando getPokemonNameId'); // Log de inicio
-
+       
         if (id) {
             if (id.length > 4) {
-                console.log('Buscando en la DB'); // Log específico para búsqueda en la DB
+               
                 const pokeDb = await getPokeDbId(id);
-                console.log('Resultado de la DB:', pokeDb); // Log del resultado de la DB
-                return pokeDb; // Añadido retorno aquí
+               
+                return pokeDb; 
             }
             urlGet = `https://pokeapi.co/api/v2/pokemon/${id}`;
         } else {
             urlGet = `https://pokeapi.co/api/v2/pokemon/${name}`;
         }
 
-        console.log('Realizando solicitud a la API:', urlGet); // Log de la solicitud a la API
+        
         const { data } = await axios.get(urlGet);
 
         const pokemon = {
@@ -33,8 +32,8 @@ const getPokemonNameId = async (id) => {
             imageDefault: data.sprites.other.dream_world.front_default,
             types: data.types.map((t) => t.type.name),
         };
-        console.log('Pokemon obtenido:', pokemon); // Log del Pokemon obtenido
-        return pokemon; // Añadido retorno aquí
+       
+        return pokemon; 
     } catch (error) {
         return({ error: error.message });
     }
@@ -42,7 +41,7 @@ const getPokemonNameId = async (id) => {
 
 const getPokeDbId = async (id) => {
     try {
-        console.log("ingresa a buscarlo en DB");
+       
         const { dataValues } = await Pokemon.findByPk(id, {
             include: {
                 model: Type,
