d5862a9908fca57b98939dd44217789b4d5068c6
Victor Sepulveda <victorseva123@gmail.com>
2024-03-13 13:41:59 -0500

    PI ok 13/03/24

--- PI Pokemon/api/src/controllers/postPokemon.js
+++ PI Pokemon/api/src/controllers/postPokemon.js
@@ -1,26 +1,27 @@
 const {Pokemon} = require("../db");
 
-const postPokemon = async (req, res) => {
+const postPokemon = async (newPok, res) => { // Agrega res como un parámetro
     try {
-        let {name, life, stroke, defending, speed, height, weight, imageDefault, type} = req;
+        let { name, life, stroke, defending, speed, height, weight, imageDefault, type } = newPok;
+
         // Carga el Pokemon creado a la database (DB)
         let pokeCreated = await Pokemon.create({
-            name, 
-            life, 
-            stroke, 
-            defending, 
-            speed, 
-            height, 
-            weight, 
+            name,
+            life,
+            stroke,
+            defending,
+            speed,
+            height,
+            weight,
             imageDefault,
             type
         });
-        return {pokeCreated}
+
+        return pokeCreated; // Devuelve el Pokémon creado
     } catch (error) {
-        res.status(404).json({error: error.messaje})
+        res.status(500).json({ error: error.message }); // Envía una respuesta de error al cliente
         console.log(error);
     }
 }
 
 module.exports = postPokemon;
-
